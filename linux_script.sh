41f1b65a4d936b0017df8aa68c7e58d734dfc2e3
nastyagrifon <neckronus@yandex.ru>
2021-09-25 16:12:16 +0400

    Added adb check

--- linux_script.sh
+++ linux_script.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# Purpose: Automating Nexus 5 Repartitioning
+# Author: Nastya Grifon <neckronus@yandex.ru>
+# Last updated on : 25-Sept-2021
+# -----------------------------------------------
+
+#check if adb is installed
+
+if ! command -v adb &> /dev/null
+then
+    echo "adb could not be found"
+    
+    exit
+fi
+
 adb devices
 adb root
 adb shell

cba9d6ad33447678e5ceb2edd01a73122a13993a
nastyagrifon <neckronus@yandex.ru>
2021-09-25 18:34:17 +0400

    Added color to outputs

--- linux_script.sh
+++ linux_script.sh
@@ -7,28 +7,33 @@
 linux_adb_link=https://dl.google.com/android/repository/platform-tools-latest-linux.zip
 platform_tools_link=https://developer.android.com/studio/releases/platform-tools
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+#echo -e"I ${RED}love${NC} Stack Overflow"
+
 #continue if adb is installed and download if it's not
 if ! command -v adb &> /dev/null ;then
     if test -f "./platform-tools/adb" && test -f "./platform-tools/fastboot"
     then 
-        echo "Downloaded adb found"
+        echo -e "${GREEN}Downloaded adb found${NC}"
         export adb='./platform-tools/adb'
         export fastboot='./platform-tools/fastboot'
     else
-        echo "Could not find adb"
-        echo -n "Do you want to download adb?"
+        echo -e "${RED}Could not find adb${NC}"
+        echo -e -n "Do you want to download adb?"
         read answer_req
         if [ "$answer_req" != "${answer_req#[Yy]}" ] ;then
             wget -O platform_tools.zip $linux_adb_link &> /dev/null
-            echo "Download complete. Extracting..."
+            echo -e "${GREEN}Download complete. Extracting...${NC}"
             bsdtar -x -f platform_tools.zip
             export adb='./platform-tools/adb'
             export fastboot='./platform-tools/fastboot'
-            echo "Successfully installed adb"
+            echo -e "${GREEN}Successfully installed adb${NC}"
             rm platform_tools.zip
         else
-            echo "The script won't work without Android Debug Bridge"
-            echo "You can download it manually at $platform_tools_link"
+            echo -e "${RED}The script won't work without Android Debug Bridge${NC}"
+            echo -e "You can download it manually at $platform_tools_link"
             exit
         fi
     fi

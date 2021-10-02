66fb0b8a3c82e7890c6aa672bb2e647455d975f8
nastyagrifon <neckronus@yandex.ru>
2021-10-02 17:26:14 +0400

    format linux script

--- linux_script.sh
+++ linux_script.sh
@@ -13,11 +13,11 @@ NC='\033[0m' # No Color
 #echo -e"I ${RED}love${NC} Stack Overflow"
 
 # Log stderr to linux.log
-exec 2> ./logs/linux.log
+exec 2>./logs/linux.log
 
 # Continue if adb is installed and download if it's not
-if ! command -v adb &> /dev/null ;then
-    if test -f "./platform-tools/adb" && test -f "./platform-tools/fastboot" ;then 
+if ! command -v adb &>/dev/null; then
+    if test -f "./platform-tools/adb" && test -f "./platform-tools/fastboot"; then
         echo -e "${GREEN}Downloaded adb found${NC}"
         export adb='./platform-tools/adb'
         export fastboot='./platform-tools/fastboot'
@@ -25,7 +25,7 @@ if ! command -v adb &> /dev/null ;then
         echo -e "${RED}Could not find adb${NC}"
         echo -e -n "Do you want to download adb?"
         read answer_req
-        if [ "$answer_req" != "${answer_req#[Yy]}" ] ;then
+        if [ "$answer_req" != "${answer_req#[Yy]}" ]; then
             wget -O platform_tools.zip $linux_adb_link >&2
             echo -e "${GREEN}Download complete. Extracting...${NC}"
             bsdtar -x -f platform_tools.zip
